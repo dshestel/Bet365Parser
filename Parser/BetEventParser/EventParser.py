cfb40ab4b312e4cbe9fcdd365fb874d0338bfb46
Дмитрий Шестель <dshestel@gmail.com>
2018-12-03 12:32:44 +0300

    fiz

--- Parser/BetEventParser/EventParser.py
+++ Parser/BetEventParser/EventParser.py
@@ -1,165 +1,222 @@
+import csv
 import time
 import os.path
 from selenium import webdriver
-import sys
 
 driver = webdriver.Chrome(os.path.dirname(os.path.abspath('')) + '\chromedriver1.exe')
 
 driver.get('https://www.bet365.com/?lng=1&rurl=casino.bet365.com#/IP/')
 driver.get('https://www.bet365.com/?lng=1&rurl=casino.bet365.com#/IP/')
 
-time.sleep(20)
+csvData = [["Time"], ["Attacks"], ["Dangerous Attacks"], ["Possession %"], ["On Target"], ["Off Target"], ["League"], ["Command Name"], ["Score"],
+          ["1x2 First command"], ["1x2 The second command"], ["1x2 Draw"], ["Next goal First command"], ["Next goal second command"], ["Next goal Draw"],
+          ["Match goals First command"], ["Match goals The second command"], ["Match goals Draw"]]
+
+time.sleep(10)
 bar_item = driver.find_elements_by_xpath("//div[@class='ip-ControlBar_BBarItem ']")
 for element in bar_item:
     if element.text == "Event View":
-        element.click();
+        element.click()
         break
 time.sleep(5)
 
-# best selector
-xpath = driver.find_elements_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div"
-                                      "[3]/div[2]/div/div/div/div[@class='ipn-FixtureButton ']")
 
-for element in xpath:
-    element.click()
-    time.sleep(3)
-    try:
-        corner = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
-                                              '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[3]')
-        # print(corner.text)
-    except:
-        print("Corner error")
+while True:
+    # for open all closed matches
+    closed_element = driver.find_elements_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]"
+                                                   "/div[1]/div/div/div/div[3]/div[2]"
+                                                   "/div[@class='ipn-Competition ipn-Competition-closed ']")
+    if len(closed_element) > 0:
+        for element in closed_element:
+            element.click()
+            time.sleep(1)
 
-    try:
-        yellow_card = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
-                                                   '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[4]')
-        # print(yellow_card.text)
-    except:
-        print("yellow_card error")
+    # best selector
+    xpath = driver.find_elements_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div"
+                                          "[3]/div[2]/div/div/div/div[@class='ipn-FixtureButton ']")
+    if len(xpath) > 0:
+        for element in xpath:
+            element.click()
+            time.sleep(3)
+            data = [[]]
+            try:
+                time = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
+                                                    '/div/div/div/div[1]/div[1]/div[2]/div[1]')
+                data = [[time.text]]
+            except:
+                print("Time error")
+                data = [['00:00']]
 
-    try:
-        red_card = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
-                                                '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[5]')
-        # print(red_card.text)
-    except:
-        print("red_card error")
+            try:
+                corner = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
+                                                      '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[3]')
+                split_corner = corner.text.split('\n')
+                data.append([split_corner[0], split_corner[1]])
+                # print(corner.text)
+            except:
+                print("Corner error")
+                data.append(["None"])
 
-    try:
-        penalty = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
-                                               '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[9]')
-        # print(penalty.text)
-    except:
-        print("penalty error")
+            try:
+                yellow_card = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
+                                                           '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[4]')
+                split_yellow_card = yellow_card.text.split('\n')
+                data.append([split_yellow_card[0], split_yellow_card[1]])
+                # print(yellow_card.text)
+            except:
+                print("yellow_card error")
+                data.append(["None"])
 
-    try:
-        goal = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
-                                            '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[10]')
-        # print(goal.text)
-    except:
-        print("goal error")
+            try:
+                red_card = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
+                                                        '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[5]')
+                split_red_card = red_card.text.split('\n')
+                data.append([split_red_card[0], split_red_card[1]])
+                # print(red_card.text)
+            except:
+                print("red_card error")
+                data.append(["None"])
 
-    try:
-        attacks = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
-                                               '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[1]/div')
-        # print(attacks.text)
-    except:
-        print("attacks error")
+            try:
+                penalty = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
+                                                       '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[9]')
+                split_penalty = penalty.text.split('\n')
+                data.append([split_penalty[0], split_penalty[1]])
+                # print(penalty.text)
+            except:
+                print("penalty error")
+                data.append(["None"])
 
-    try:
-        danger_attacks = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
-                                                      '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[2]')
-        # print(danger_attacks.text)
-    except:
-        print("danger_attacks error")
+            try:
+                goal = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
+                                                    '/div/div/div/div[1]/div[1]/div[2]/div[3]/div[10]')
+                split_goal = goal.text.split('\n')
+                data.append([split_goal[0], split_goal[1]])
+                # print(goal.text)
+            except:
+                print("goal error")
 
-    try:
-        possession = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
-                                                  '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[3]')
-        # print(possession.text)
-    except:
-        print("possession error")
+            try:
+                left_attacks = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
+                                                            '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[1]'
+                                                            '/div/div[1]')
 
-    try:
-        on_target = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
-                                                 '/div/div/div/div[3]/div/div/div/div[3]/div[4]/div[1]/div')
-        # print(on_target.text)
-    except:
-        print("on_target error")
+                right_attacks = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
+                                                             '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[1]'
+                                                             '/div/div[3]')
 
-    try:
-        off_target = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
-                                                  '/div/div/div/div[3]/div/div/div/div[3]/div[4]/div[2]/div')
-        # print(off_target.text)
-    except:
-        print("off_target error")
+                data.append([left_attacks.text, right_attacks.text])
+                # print(attacks.text)
+            except:
+                print("attacks error")
+                data.append(["None"])
 
-    try:
-        full_time_result = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                        '/div[2]/div/div/div/div[3]/div[2]/div[2]')
-        # print(full_time_result.text)
-    except:
-        print("full_time_result error")
+            try:
+                danger_attacks = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
+                                                              '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[2]')
+                #danger_attacks =
+                # print(danger_attacks.text)
+            except:
+                print("danger_attacks error")
+                data.append(["None"])
 
-    try:
-        double_chance = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                     '/div[2]/div/div/div/div[3]/div[3]/div[2]')
-        # print(double_chance.text)
-    except:
-        print("double_chance error")
+            try:
+                possession = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
+                                                          '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[3]')
+                # print(possession.text)
+            except:
+                print("possession error")
+                data.append(["None"])
 
-    try:
-        half_time_result = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                        '/div[2]/div/div/div/div[3]/div[4]/div[2]/div')
-        # print(half_time_result.text)
-    except:
-        print("half_time_result error")
+            try:
+                on_target = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
+                                                         '/div/div/div/div[3]/div/div/div/div[3]/div[4]/div[1]/div')
+                # print(on_target.text)
+            except:
+                print("on_target error")
+                data.append(["None"])
 
-    try:
-        first_goal = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                  '/div[2]/div/div/div/div[3]/div[5]/div[2]/div')
-        # print(first_goal.text)
-    except:
-        print("first_goal error")
+            try:
+                off_target = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
+                                                          '/div/div/div/div[3]/div/div/div/div[3]/div[4]/div[2]/div')
+                # print(off_target.text)
+            except:
+                print("off_target error")
+                data.append(["None"])
 
-    try:
-        match_goals = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                   '/div[2]/div/div/div/div[3]/div[6]/div[2]/div')
-        # print(match_goals.text)
-    except:
-        print("match_goals error")
+            try:
+                full_time_result = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
+                                                                '/div[2]/div/div/div/div[3]/div[2]/div[2]')
+                # print(full_time_result.text)
+            except:
+                print("full_time_result error")
+                data.append(["None"])
 
-    try:
-        alternative_match_goals = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                               '/div[2]/div/div/div/div[3]/div[7]/div[2]/div')
-        # print(alternative_match_goals.text)
-    except:
-        print("alternative_match_goals error")
+            try:
+                double_chance = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
+                                                             '/div[2]/div/div/div/div[3]/div[3]/div[2]')
+                # print(double_chance.text)
+            except:
+                print("double_chance error")
+                data.append(["None"])
 
-    try:
-        first_half_goals = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                        '/div[2]/div/div/div/div[3]/div[8]/div[2]/div')
-        # print(first_half_goals.text)
-    except:
-        print("first_half_goals error")
+            try:
+                half_time_result = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
+                                                                '/div[2]/div/div/div/div[3]/div[4]/div[2]/div')
+                # print(half_time_result.text)
+            except:
+                print("half_time_result error")
+                data.append(["None"])
 
-    try:
-        asian_handicap = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                      '/div[2]/div/div/div/div[3]/div[9]/div[2]/div')
-        print(asian_handicap.text)
-    except:
-        print("asian_handicap error")
+            try:
+                first_goal = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
+                                                          '/div[2]/div/div/div/div[3]/div[5]/div[2]/div')
+                # print(first_goal.text)
+            except:
+                print("first_goal error")
+                data.append(["None"])
 
-    try:
-        goal_line = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
-                                                 '/div[2]/div/div/div/div[3]/div[10]/div[2]/div')
-        # print(goal_line.text)
-    except:
-        print("goal_line error")
+            try:
+                match_goals = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
+                                                           '/div[2]/div/div/div/div[3]/div[6]/div[2]/div')
+                # print(match_goals.text)
+            except:
+                print("match_goals error")
+                data.append(["None"])
 
+            try:
+                alternative_match_goals = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div'
+                                                                       '/div[2]/div[2]/div/div/div/div[3]/div[7]'
+                                                                       '/div[2]/div')
+                # print(alternative_match_goals.text)
+            except:
+                print("alternative_match_goals error")
+                data.append(["None"])
+
+            try:
+                first_half_goals = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
+                                                                '/div[2]/div/div/div/div[3]/div[8]/div[2]/div')
+                # print(first_half_goals.text)
+            except:
+                print("first_half_goals error")
+                data.append(["None"])
+
+            try:
+                asian_handicap = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
+                                                              '/div[2]/div/div/div/div[3]/div[9]/div[2]/div')
+                print(asian_handicap.text)
+            except:
+                print("asian_handicap error")
+                data.append(["None"])
+
+            try:
+                goal_line = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]'
+                                                         '/div[2]/div/div/div/div[3]/div[10]/div[2]/div')
+                # print(goal_line.text)
+            except:
+                print("goal_line error")
+                data.append(["None"])
+
+    else:
+        print("else")
     # break
-
-# print(len(xpath))
-#
-# for element in xpath:
-#     element.click()
-#     time.sleep(2)
--- Parser/ParserFolder/Test.py
+++ Parser/ParserFolder/Test.py
@@ -1,28 +1,6 @@
-# import time
-# from selenium import webdriver
-#
-# driver = webdriver.Chrome('D:/chromedriver1.exe')
-#
-# #driver.get('https://mobile.bet365.com/#type=InPlay;key=;ip=1;lng=1')
-# driver.get('https://www.bet365.com/?lng=1&rurl=casino.bet365.com#/IP/')
-# driver.get('https://www.bet365.com/?lng=1&rurl=casino.bet365.com#/IP/')
-#
-# #<div class="ipo-Classification sport_1 "><h4 class="ipo-Classification_Name">Soccer</h4></div>
-# while 1:
-#     time.sleep(5)
-#     elements = driver.find_elements_by_xpath("//div[@class='wl-MediaButtonLoader wl-MediaButtonLoader_ML1 ']")
-#     if len(elements) == 0:
-#         # <div class="ip-ControlBar_BBarItem " style="">Overview</div>
-#         elements1 = driver.find_elements_by_xpath("//div[@class='ip-ControlBar_BBarItem ']")
-#         print(elements1[0].text)
-#         #if len(elements1) > 0:
-#         #    elements1[0].click()
-
-
 import time
 import os.path
 from selenium import webdriver
-
 
 driver = webdriver.Chrome(os.path.dirname(os.path.abspath('')) + '\chromedriver1.exe')
 
@@ -37,31 +15,40 @@ for element in bar_item:
         break
 time.sleep(10)
 
-#useless
-#elements = driver.find_elements_by_xpath("//div[@class='ipn-ClassificationContainer ']")
-#my = driver.find_elements_by_xpath("//div[@class='ipn-Competition ']")
-#sockerMatches = driver.find_elements_by_xpath("//div[@class='ipn-Fixture ipn-Fixture_HasTeamStack ipn-Fixture-closed ipn-Fixture-hastimer ']")
-#sockerMatches1 = driver.find_elements_by_xpath("//div[@class='ipn-ClassificationButton ipn-ClassificationButton_Classification-1 ']")
-#useless
-
-#best option
-opening = driver.find_elements_by_xpath("//div[@class='ipn-Classification ipn-Classification-open ']")
-#best option
-
-#try
-xpath = driver.find_elements_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[1]"
-                                      "/div/div/div/div[3]/div[2]/div")
+# xpath = driver.find_elements_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div"
+#                                       "[3]/div[2]/div/div/div/div[@class='ipn-FixtureButton ']")
+#
+# time = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[2]'
+#                                     '/div/div/div/div[1]/div[1]/div[2]/div[1]')
 #
 
-print(len(xpath))
-for element in xpath:
-    print(element.text)
-#print(len(opening))
-#print(opening[0].text)
+# closed_element = driver.find_elements_by_xpath("/html/body/div[1]/div/div[2]/div[1]/div/div/div[2]"
+#                                                "/div[1]/div/div/div/div[3]/div[2]"
+#                                                "/div[@class='ipn-Competition ipn-Competition-closed ']")
+#
+# for element in closed_element:
+#     element.click()
+#     time.sleep(2)
+#
+# print(len(closed_element))
 
-# s = elements[0].text.split("\n")
-# print(len(s))
-# print(len(my))
-# print(len(sockerMatches))
-# for element in sockerMatches:
-#     print(element.text)
+# print(time.text)
+
+left_attacks = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
+                                            '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[1]/div/div[1]')
+right_attacks = driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]'
+                                             '/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[1]/div/div[3]')
+
+
+# /html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[1]/div
+# /html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[1]/div/div[1]
+# /html/body/div[1]/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div[3]/div/div/div/div[3]/div[2]/div[1]/div/div[3]
+#sp = penalty.text.split('\n')
+#print(sp[0] + ":" + sp[1])
+
+print(left_attacks.text)
+print(right_attacks.text)
+
+
+#<div class="ipn-Competition ipn-Competition-closed "><div class="ipn-CompetitionButton " style="">Spain Segunda B</div><div class="ipn-Competition_Icon "></div><div class="ipn-Competition_FavouriteButton "></div></div>
+# /html/body/div[1]/div/div[2]/div[1]/div/div/div[2]/div[1]/div/div/div/div[3]/div[2]/div[@class='ipn-Competition ipn-Competition-closed ']
